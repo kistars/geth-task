53212f4fd845eb129c48704d5aa2acee4710070a
kistars <sunllen2014@outlook.com>
2025-07-01 22:33:44 +0800

    task 1

---
+++ constant/constant.go
@@ -0,0 +1,7 @@
+package constant
+
+const (
+	SepoliaAddr      = "https://eth-sepolia.g.alchemy.com/v2/N06wr1E4Jprn4Ojne7Xz5h3K20ek3spx"
+	SenderPrivateKey = "5e85790b08a48921ebb20230286fbf217db94181472be71f6ff5c9cde3170c66"
+	ReceiverAddr     = "0x74eDf70e292bA5b835FAf90a164e02357a92FC89"
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kistars/geth-task/task1"
+)
+
+func main() {
+	task1.ExecTask1()
+}
